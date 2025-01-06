38a3d609c185d882c36920bee2d0584ed8bbae93
Paul Marechal <paulmarechal785@hotmail.fr>
2025-01-06 11:07:06 +0100

    Update images + css images - Desktop

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -227,11 +227,11 @@ const Desktop = () => {
                             Afin que vous soyez parfaitement défendu, une convention d’honoraires sera signée dès le début de la procédure, conformément aux règles déontologiques de la profession d’avocat. 
                         </p>
                     </div>
-                    <div className="paris_monument"></div>
                 </section>
 
                 <section>
                     <div className="main_div_contact_me_bottom_icon">
+                    <div className="paris_monument"></div>
                         <div className="icon_bottom_page">
                             <a href="http://maps.google.com/?q=Pl Charles de Gaulle, Paris" target="_blank" rel="noopener noreferrer" title="Pour me rencontrer au bureau">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="#007BEE"  className="icon icon-tabler icons-tabler-filled icon-tabler-location"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20.891 2.006l.106 -.006l.13 .008l.09 .016l.123 .035l.107 .046l.1 .057l.09 .067l.082 .075l.052 .059l.082 .116l.052 .096c.047 .1 .077 .206 .09 .316l.005 .106c0 .075 -.008 .149 -.024 .22l-.035 .123l-6.532 18.077a1.55 1.55 0 0 1 -1.409 .903a1.547 1.547 0 0 1 -1.329 -.747l-.065 -.127l-3.352 -6.702l-6.67 -3.336a1.55 1.55 0 0 1 -.898 -1.259l-.006 -.149c0 -.56 .301 -1.072 .841 -1.37l.14 -.07l18.017 -6.506l.106 -.03l.108 -.018z" /></svg>
